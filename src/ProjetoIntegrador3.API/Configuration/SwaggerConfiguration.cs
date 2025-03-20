4d4d30c8c80d278941b7475df13831ea4dcd012a
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2025-03-20 11:53:11 -0300

    Extraindo configuracoes do swagger

---
+++ src/ProjetoIntegrador3.API/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,21 @@
+namespace ProjetoIntegrador3.API.Configuration;
+
+public static class SwaggerConfiguration
+{
+    public static WebApplicationBuilder AddSwaggerConfiguration(this WebApplicationBuilder builder)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        return builder;
+    }
+
+    public static WebApplication UseSwaggerConfiguration(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        
+        return app;
+    }
+}
