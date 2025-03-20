59860e00fe00d01a823fbe0c2100fb76bfbf6f11
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2025-03-20 15:06:10 -0300

    Habilitando swagger no ambiente de staging

--- src/ProjetoIntegrador3.API/Program.cs
+++ src/ProjetoIntegrador3.API/Program.cs
@@ -19,7 +19,7 @@ builder.AddDependencyInjectionConfiguration();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.UseSwaggerConfiguration();
 }
