48e458168b72a9d03c8ab57f22dc6d4c399e8180
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2025-04-01 21:16:32 -0300

    Criando repositorio de Genre

---
+++ src/ProjetoIntegrador3.Infra.Data/Repository/GenreRepository.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoIntegrador3.Domain.Interfaces;
+using ProjetoIntegrador3.Domain.Models;
+using ProjetoIntegrador3.Infra.Data.Context;
+
+namespace ProjetoIntegrador3.Infra.Data.Repository;
+
+public class GenreRepository : IGenreRepository
+{
+    protected readonly PiApplicationDbContext Db;
+    protected readonly DbSet<Genre> DbSet;
+
+    public GenreRepository(DbSet<Genre> dbSet, PiApplicationDbContext dbContext)
+    {
+        Db = dbContext;
+        DbSet = Db.Set<Genre>();
+    }
+
+    public async Task<Genre> GetById(int id)
+    {
+        return await DbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Genre>> GetAll()
+    {
+        return await DbSet.OrderBy(x => x.Name).ToListAsync();
+    }
+
+    public void CreateGenre(Genre genre)
+    {
+        DbSet.Add(genre);
+    }
+
+    public void UpdateGenre(Genre genre)
+    {
+        DbSet.Update(genre);
+    }
+
+    public void DeleteGenre(Genre genre)
+    {
+        DbSet.Remove(genre);
+    }
+
+    private bool disposed = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Db.Dispose();
+            }
+        }
+        disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
