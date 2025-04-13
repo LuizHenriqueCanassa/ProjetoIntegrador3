f89a6e0770ea86d41a371de4ce892a95ec73bf50
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2025-04-13 16:57:51 -0300

    Logout

--- src/ProjetoIntegrador3.Infra.Identity/Authorization/RequirementRoleClaimFilter.cs
+++ src/ProjetoIntegrador3.Infra.Identity/Authorization/RequirementRoleClaimFilter.cs
@@ -1,20 +1,46 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using ProjetoIntegrador3.Domain.Models;
+using ProjetoIntegrador3.Infra.Identity.JWT;
 
 namespace ProjetoIntegrador3.Infra.Identity.Authorization;
 
 public class RequirementRoleClaimFilter : IAuthorizationFilter
 {
     private readonly Claim _claim;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly AppJwtSettings _appJwtSettings;
 
-    public RequirementRoleClaimFilter(Claim claim)
+    public RequirementRoleClaimFilter(Claim claim, UserManager<ApplicationUser> userManager, IOptions<AppJwtSettings> appJwtSettings)
     {
         _claim = claim;
+        _userManager = userManager;
+        _appJwtSettings = appJwtSettings.Value;
     }
 
     public void OnAuthorization(AuthorizationFilterContext context)
     {
+        var user = _userManager.FindByNameAsync(context.HttpContext.User.Identity.Name).Result;
+        
+        context.HttpContext.Request.Headers.TryGetValue("Authorization", out var jwtToken);
+
+        var token = _userManager.GetAuthenticationTokenAsync(user, _appJwtSettings.Issuer, "JWT").Result;
+
+        if (token == null)
+        {
+            context.Result = new StatusCodeResult(401);
+            return;
+        }
+        
+        if (jwtToken.ToString().Replace("Bearer ", "") != token)
+        {
+            context.Result = new StatusCodeResult(401);
+            return;
+        }
+        
         if (context.HttpContext.User.Identity == null)
             throw new InvalidOperationException();
 
