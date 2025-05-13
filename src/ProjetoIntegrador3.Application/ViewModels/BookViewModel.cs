f89a4f0fe466a4962ef2f2b0df3b0d8cdc9c9af7
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2025-05-12 22:10:54 -0300

    Adicionando Id no response dos livros

--- src/ProjetoIntegrador3.Application/ViewModels/BookViewModel.cs
+++ src/ProjetoIntegrador3.Application/ViewModels/BookViewModel.cs
@@ -2,6 +2,7 @@ namespace ProjetoIntegrador3.Application.ViewModels;
 
 public class BookViewModel
 {
+    public int Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
